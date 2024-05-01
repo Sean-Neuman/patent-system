e73d5bee55399757a7b75541486e5b2d4f17ce57
Sean Neuman <srn5207@psu.edu>
2024-05-01 19:53:52 -0400

    update

--- pages/_app.js
+++ pages/_app.js
@@ -5,29 +5,37 @@ import {
   LoginButton,
 } from "../components/StyledNavbar";
 import { GlobalStyles } from "@/styles/GlobalStyles";
-import { ChainId, ThirdwebProvider } from "@thirdweb-dev/react";
+import {
+  ChainId,
+  ThirdwebProvider,
+  ConnectWallet,
+  metamaskWallet,
+  coinbaseWallet,
+  walletConnect,
+} from "@thirdweb-dev/react";
 import { ThemeProvider } from "styled-components";
-import { lightTheme, darkTheme } from "../styles/theme";  
-
-const activeChainId = ChainId.Mainnet;
+import { lightTheme, darkTheme } from "../styles/theme";
 
 function App({ Component, pageProps }) {
   return (
-    <ThirdwebProvider desiredChainId={activeChainId}>
-      <ThemeProvider theme={darkTheme}> 
+    <ThemeProvider theme={darkTheme}>
       <GlobalStyles />
-      <NavbarContainer>
-        <NavbarLinks>
-          <a href="/">Home</a>
-          <a href="/ProfilePage">Profile</a>
-          <a href="/explore">Explore</a>
-          <a href="/create-patent">Create Patent</a>
-        </NavbarLinks>
-        <LoginButton>Login</LoginButton>
-      </NavbarContainer>
-      <Component {...pageProps} />
-      </ThemeProvider>
-    </ThirdwebProvider>
+      <ThirdwebProvider
+        activeChain="binance-testnet"
+        clientId="56a391e3dfd93b02d43834363461a36c"
+      >
+        <NavbarContainer>
+          <NavbarLinks>
+            <a href="/">Home</a>
+            <a href="/ProfilePage">Profile</a>
+            <a href="/explore">Explore</a>
+            <a href="/create-patent">Create Patent</a>
+          </NavbarLinks>
+          <ConnectWallet />
+        </NavbarContainer>
+        <Component {...pageProps} />
+      </ThirdwebProvider>
+    </ThemeProvider>
   );
 }
 export default App;
